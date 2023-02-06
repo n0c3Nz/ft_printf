c5e56f8bdc2e1380e3368540e2f3843869c7b791
4g4x0r <106092515+4g4x0r@users.noreply.github.com>
2023-02-06 12:15:48 +0100

    abierto

---
+++ libft/ft_putaddr_fd.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+int	ft_putaddr_fd(unsigned long int n, int fd)
+{
+	static int	i;
+	char		*base;
+
+	base = "0123456789abcdef";
+	i = 0;
+	if (n >= 16)
+		ft_putaddr_fd(n / 16, fd);
+	n %= 16;
+	write (fd, &base[n], 1);
+	return (i++);
+}
